45c5504be71e5a5d641276b68ad8f8a3f90b1e3f
shark <thetruephil@googlemail.com>
2017-12-19 22:52:05 +0100

    initial commit

---
+++ climex-r-ubuntu/requirements.R
@@ -0,0 +1,10 @@
+## Install all images required in the Docker image
+install.packages( 
+    c( "dplyr", "xts", "devtools", "ggplot2", "tidyr",
+      "tibble", "latex2exp", "lubridate", "bookdown", "pander",
+      "Rcpp", "RcppArmadillo", "markdown", "dygraphs", "shiny",
+      "htmltools", "shinydashboard", "shinytoastr", "RCurl",
+      "RColorBrewer", "deseasonalize", "Lmoments", "moments",
+      "leaflet", "numDeriv", "alabama",
+      repo = "https://cloud.r-project.org" )
+
